79967b519da6e34e0fda4d6792cd12748cce961e
Nathan Sculli <scull7@gmail.com>
2016-07-17 23:17:17 -0700

    Refactor!

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 
-const _         = require('ramda');
-const Check     = require('./lib/check');
-const Field     = require('./lib/field');
-const Action    = require('./lib/action');
+const _                 = require('ramda');
+const Check             = require('./lib/check');
+const Field             = require('./lib/field');
+const Action            = require('./lib/action');
+
+const MSG_USER_CTX      = 'context parameter (ctx) must be an object';
+const MSG_FIELD_FACTORY = 'field_factory parameter must be a function';
+const MSG_FIELD_CONFIG  = 'field_factory must return an object';
+
+
+const checkParam = (test, msg) =>
+  _.unless(test, () => { throw new TypeError(msg) });
+
+
+const parseActions = (user_ctx, field_factory) => _.compose(
+  _.values
+, Field.configToActionList
+, checkParam(_.is(Object), MSG_FIELD_CONFIG)
+, field_factory
+)(Check, user_ctx)
 
 
 const Awesomize = (user_ctx, field_factory) => {
-  if (typeof user_ctx !== 'object') {
-    throw new TypeError('context parameter (ctx) must be an object');
-  }
+  checkParam(_.is(Object), MSG_USER_CTX)(user_ctx)
+  checkParam(_.is(Function), MSG_FIELD_FACTORY)(field_factory);
 
-  if (typeof field_factory !== 'function') {
-    throw new TypeError('field_factory parameter must be a function');
-  }
+  const actions = parseActions(user_ctx, field_factory);
 
-  const field_config = field_factory(Check, user_ctx);
-
-  if (!_.is(Object, field_config)) {
-    throw new TypeError('field_factory must return an object');
-  }
-
-  const actions      = Field.configToActionList(field_config);
-
-  return (request, current) => {
-    const ctx = { request, current };
-
-    return Action.Runner(ctx, _.values(actions));
-  };
+  return (request, current) => Action.Runner({ request, current }, actions);
 };
 
 Awesomize.MSG  = Check.MSG;
